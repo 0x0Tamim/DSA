3659da2abc906f4275bb549733780fc487acc7b8
0x0Tamim <mmtamimuzzaman@gmail.com>
2025-08-11 01:11:01 +0600

    LCS using Recursion code modified

--- LCS using Recursion/src/Main.java
+++ LCS using Recursion/src/Main.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Main {
 
     static int lcsRec(String a, String b, int i, int j) {
-        if (i == 0 || j == 0) return 0; // Base case
-        if (a.charAt(i - 1) == b.charAt(j - 1))
-            return 1 + lcsRec(a, b, i - 1, j - 1);
-        else
-            return Math.max(lcsRec(a, b, i - 1, j), lcsRec(a, b, i, j - 1));
+      if(i==0||j==0) return 0;
+      if(a.charAt(i-1) == b.charAt(j-1))
+          return 1+lcsRec(a,b,i-1,j-1);
+      else
+          return Math.max(lcsRec(a,b,i,j-1),lcsRec(a,b,i-1,j));
     }
 
     public static void main(String[] args) {
@@ -19,9 +19,8 @@ public class Main {
         System.out.print("Enter second string: ");
         String b = sc.nextLine();
 
-        int result = lcsRec(a, b, a.length(), b.length());
-        System.out.println("Length of LCS: " + result);
+        System.out.println("Size of LCS = "+lcsRec(a,b,a.length(),b.length()));
 
-        sc.close();
+
     }
 }
