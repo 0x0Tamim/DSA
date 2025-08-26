63fc5aa5ea3a383c1c7c2bd6394e9433df41a0d5
0x0Tamim <mmtamimuzzaman@gmail.com>
2025-08-26 06:55:49 +0600

    Weighted Directed Graph code added

---
+++ Weighted Directed Graph/src/Main.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of vertices: ");
+        int V = sc.nextInt();
+
+        ArrayList<ArrayList<int[]>>mat = new ArrayList<>();
+
+        for(int i = 0;i<V;i++){
+            mat.add(new ArrayList<>());
+        }
+
+        System.out.print("Enter number of edges: ");
+        int E = sc.nextInt();
+
+        System.out.println("Enter edges u v and weight w(u v w)");
+        for(int i = 0;i<E;i++){
+            int u = sc.nextInt();
+            int v = sc.nextInt();;
+            int w = sc.nextInt();
+            AddEdgeWithWeight(mat,u,v,w);
+        }
+
+        printGraph(mat);
+
+    }
+
+    static void AddEdgeWithWeight(ArrayList<ArrayList<int[]>>adj,int u,int v,int w){
+        adj.get(u).add(new int[] {v,w});
+
+    }
+
+    static void printGraph(ArrayList<ArrayList<int[]>>adj){
+        for (int i = 0; i < adj.size(); i++) {
+            System.out.print(i + " --> ");
+            for (int j = 0; j < adj.get(i).size(); j++) {
+                int[] edge = adj.get(i).get(j);
+                System.out.print("(" + edge[0] + ", w=" + edge[1] + ")");
+                if (j < adj.get(i).size() - 1) System.out.print(", ");
+            }
+            System.out.println();
+        }
+    }
+}
