93bc4e0b1cd5504d6db2dea2ba3de63bf29cee7f
0x0Tamim <mmtamimuzzaman@gmail.com>
2025-09-15 23:44:48 +0600

    Selection Sort code modified

--- Selection Sort/src/Main.java
+++ Selection Sort/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
        Scanner sc = new Scanner(System.in);
         System.out.print("Enter length of array: ");
         int n = sc.nextInt();
-        System.out.print("Enter array elements to sort the arry:");
+        System.out.print("Enter array elements to sort the arry: ");
         int[] arr = new int[n];
         for(int i = 0;i<n;i++){
             arr[i] = sc.nextInt();
@@ -15,27 +15,28 @@ public class Main {
         System.out.println("Sorted array: "+Arrays.toString(arr));
 
     }
-    static void selectionSort(int[]arr){
-        for (int i = 0;i<arr.length;i++){
-            int last = arr.length-1-i;
-            int maxIndex = getMax(arr,last);
-            swap(arr,maxIndex,last);
+   static void selectionSort(int[]arr){ // Selection Sort by selecting maximum and placing it at the end
+
+       for(int i = 0;i< arr.length;i++){
+            int lastIndex = arr.length-1-i;
+            int maxIndex = getMax(arr,lastIndex);
+            swap(arr,maxIndex,lastIndex);
         }
-    }
-    public static int getMax(int[]arr,int lastIndx){
+   }
+
+   static int getMax(int[]arr,int last){
         int max = 0;
-        for(int i = 1;i<= lastIndx;i++){
-            if(arr[i]>arr[max]){
+        for(int i = 0;i<=last;i++){
+            if(arr[i]>arr[max])
                 max = i;
-            }
         }
         return max;
-    }
+   }
 
-    public static void swap(int[]arr,int p,int q){
+   static void swap(int[]arr,int p,int q){
         int tmp = arr[p];
-        arr[p] = arr[q];
-        arr[q] = tmp;
-    }
+        arr[p]=arr[q];
+        arr[q]=tmp;
+   }
 
 }
